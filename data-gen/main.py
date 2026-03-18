0b975fadf9853080af57a54d31ef6f4260899ee7
João Oliveira <joaopedrooliveira6704@gmail.com>
2026-03-18 19:02:16 +0000

    fix: Full user account creation
    Faker now creates trustful account-like mock data
    
    Closes Issue #1

--- data-gen/main.py
+++ data-gen/main.py
@@ -1,7 +1,25 @@
 from faker import Faker
+import uuid
+import csv 
 
 fake = Faker()
 
-for i in range(20):
-    print(f"Person {i+1}: {fake.first_name()} {fake.last_name()}")
-    print(fake.email(safe=True,domain="company.com"))
+file_path = './data-gen/users.csv'
+
+with open(file_path, 'w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    
+    writer.writerow(["id", "firstName", "lastName", "email", "password"])
+    
+    for i in range(20):
+        firstName = fake.first_name()
+        lastName = fake.last_name()
+        
+        email = f"{firstName}.{lastName}@{fake.free_email_domain()}"
+        password = fake.password(length=12)
+        user_id = uuid.uuid4()
+
+        writer.writerow([user_id, firstName, lastName, email, password])
+        print(f"User {i+1}: {firstName} {lastName}\nEmail:{email}\n")
+
+print(f"\nDone! Created 20 users in {file_path}")
