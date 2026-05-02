5cdc2142a69887fb032aa8a0efbcf60c1863c95b
migueljcneto <40240358@esmad.ipp.pt>
2026-05-02 18:24:07 +0100

    feat:route protection, ownership check function modifications to handle multiple params

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -10,7 +10,12 @@ import {
   removeVehicle,
   editVehicle,
 } from "../controllers/user.controller.js";
-import { protect, requireRole } from "../middleware/auth.middleware.js";
+import {
+  checkOwnership,
+  protect,
+  requireRole,
+} from "../middleware/auth.middleware.js";
+import userModel from "../models/user.model.js";
 
 const router = Router();
 
@@ -21,7 +26,13 @@ router.delete("/:id", protect, requireRole("admin"), deleteUser);
 router.get("/me", protect, getCurrentUser);
 router.get("/:id", protect, getUserById);
 router.patch("/me", protect, updateOwnUser);
-router.patch("/users/:id/role", protect, requireRole("admin"), updateRole);
+router.patch(
+  "/users/:id/role",
+  protect,
+  requireRole("admin", "company-manager"),
+  checkOwnership(userModel),
+  updateRole,
+);
 router.patch("/users/:id", protect, requireRole("admin"), updateUser);
 router.post("/me/vehicles", protect, addVehicle);
 router.patch("/me/vehicles/:plate", protect, editVehicle);
